ed3fa47cbb0de19e8fdb73811267068f00247f2f
Danlowlows <37116532+Danlowlows@users.noreply.github.com>
2020-03-04 16:18:23 -0800

    Update MobuSwapCurves.py
    Fixed issue: Copy curve function did not copy a separate instance of the curve, it copied a reference to the curve. Change now serializes the curve and rewrites it.

--- MobuSwapCurves.py
+++ MobuSwapCurves.py
@@ -4,18 +4,21 @@ def GetSelected():
     return [obj for obj in FBSystem().Scene.Components if obj.Selected and (isinstance(obj, FBModel) or isinstance(obj, FBModelSkeleton) or isinstance(obj, FBModelNull) or isinstance(obj, FBModelMarker))]
 
 def GetCurve(obj, propName, sourceDof):
-    return obj.PropertyList.Find(propName).GetAnimationNode().Nodes[sourceDof].FCurve
+    return [[key.Time, key.Value] for key in obj.PropertyList.Find(propName).GetAnimationNode().Nodes[sourceDof].FCurve.Keys]
 
-def SetCurve(obj, propName, targetDof, sourceCurve):
-    obj.PropertyList.Find(propName).GetAnimationNode().Nodes[targetDof].FCurve = sourceCurve
+def SetCurve(obj, propName, targetDof, keyInfo):
+    fcurve = obj.PropertyList.Find(propName).GetAnimationNode().Nodes[targetDof].FCurve
+    fcurve.EditClear()
+    for key in keyInfo:
+        fcurve.KeyAdd(key[0], key[1])
 
 def SwapCurves(propName, dof1, dof2):
     selectedList = GetSelected()
     for obj in selectedList:
-        curve1 = GetCurve(obj, propName, dof1)
-        curve2 = GetCurve(obj, propName, dof2)
-        SetCurve(obj, propName, dof1, curve2)
-        SetCurve(obj, propName, dof2, curve1)
+        keyInfo1 = GetCurve(obj, propName, dof1)
+        keyInfo2 = GetCurve(obj, propName, dof2)
+        SetCurve(obj, propName, dof1, keyInfo2)
+        SetCurve(obj, propName, dof2, keyInfo1)
 
 def SwapTransXY():
     SwapCurves("Lcl Translation", 0, 1)
