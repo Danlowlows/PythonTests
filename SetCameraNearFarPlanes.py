6418a5904d33783a174a0713d4ee5da869d6fd75
Danlowlows <37116532+Danlowlows@users.noreply.github.com>
2022-02-08 09:34:48 -0800

    Add files via upload

---
+++ SetCameraNearFarPlanes.py
@@ -0,0 +1,14 @@
+from pyfbsdk import FBSystem, FBApplication
+
+def SetCameraFarPlane():
+    for camera in FBSystem().Scene.Cameras:
+        camera.NearPlaneDistance = 10.0
+        camera.FarPlaneDistance = 1000000.0
+
+def CameraFarPlaneCallback(pCaller, pEvent):
+    SetCameraFarPlane()
+
+def SetCameraFarPlaneCallback():
+    app = FBApplication()
+    app.OnFileNewCompleted.Add(CameraFarPlaneCallback)
+    app.OnFileOpenCompleted.Add(CameraFarPlaneCallback)
